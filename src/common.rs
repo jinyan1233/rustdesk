070e959084a52730163daba9ad24f9821e59744b
jinyan1233 <49976684+jinyan1233@users.noreply.github.com>
2025-06-12 23:27:22 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1003,7 +1003,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://remote.tz-youyou.com:21114".to_owned()
 }
 
 #[inline]
